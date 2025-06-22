a2015b0c7a4028816b6fd6af1608e96442050dfc
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-06-22 07:13:25 +0530

    Create Singleton Pattern

--- Week1/Design_Patterns_Principles/ex1_Singleton_Pattern/Code.java
+++ Week1/Design_Patterns_Principles/ex1_Singleton_Pattern/Code.java
@@ -1,1 +1,34 @@
-// dummy file 
+public class Logger {
+    // implementing singleton using lazy initialization (created only when needed)
+    private static Logger instance; //static instance
+    
+    // private constructor
+    private Logger() {
+        // empty
+    }
+
+    // method to get instance of logger class
+    public static Logger getlogger() {
+        if (instance == null) {   // created only once for the first time 
+            instance = new Logger(); 
+        }
+
+        return instance;
+    }
+}
+
+class SingletonPatternExample {
+    public static void main(String[] args) {
+        // test case to verify there exists only one inntance of the class
+        Logger logger1 = Logger.getlogger(); // instance got initialised
+        Logger logger2 = Logger.getlogger(); // same instance is returned
+
+        if (logger1 == logger2) {
+
+            System.out.println("Logger is Singlenton !");
+        } else {
+            System.out.println("Logger isn't Singleton !");
+
+        }
+    }
+}
