5af8c2694e1fbcb4c056fee476074cd388cc02a3
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-06-28 21:21:05 +0530

    Adding scenario1 of ex2

---
+++ Week2/PL_SQL/ex2_Stored_Procedures/Scenario1/Code.sql
@@ -0,0 +1,17 @@
+-- Scenario 1
+    -- Applying 1% interest rate to all savings accounts
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest IS    
+BEGIN
+    UPDATE accounts
+    SET balance = balance + balance*0.01 , lastmodified = sysdate
+    WHERE accounttype = 'Savings';
+    
+    COMMIT;
+    
+END;
+/
+
+-- executing stored procedure
+EXEC ProcessMonthlyInterest;
+
+    
