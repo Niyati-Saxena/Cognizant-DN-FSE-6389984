05d0ac5c31e3f80bcfc51e9cde7fe0c1e6383151
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-06-29 10:16:58 +0530

    Setting up JUnit

---
+++ Week2/TDD_using_JUnit5_Mockito/JUnit/ex1_settingUp_JUnit/Code.java
@@ -0,0 +1,11 @@
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class FirstTestClass {
+
+    @Test
+    public void testAdd() {
+        int result = 2 + 3;
+        assertEquals(5 , result);
+    }
+}
