c413d9e596e8c884e047a403d5267c84ae410015
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-06-29 19:13:37 +0530

    Update Code.java

--- Week2/TDD_using_JUnit5_Mockito/Mockito/ex1_Mocking_Stubbing/Code.java
+++ Week2/TDD_using_JUnit5_Mockito/Mockito/ex1_Mocking_Stubbing/Code.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-import org.mockito.Mockito;
 
 public class MyServiceTest {
     @Test
