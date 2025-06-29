fa9c4499db6eaa63434eec6b39826c2577d2efad
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-06-29 12:20:46 +0530

    Update Code.java

--- Week2/TDD_using_JUnit5_Mockito/JUnit/ex3_Assertions_in_JUnit/Code.java
+++ Week2/TDD_using_JUnit5_Mockito/JUnit/ex3_Assertions_in_JUnit/Code.java
@@ -1,18 +1,21 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class AssertionTest {
-
     @Test
     public void testAssertions() {
         // assert equals
-        assertEquals(7 , 2 + 9);
+        assertEquals(7 , 2 + 5);
 
         // assert null
         assertNull(null);
 
         // assert not null
-        assertNotNull();
+        assertNotNull(new Object());
 
+        // assert true
+        assertTrue(2==2);
+
+        // assert false
+        assertFalse(3 == 7);
     }
-
 }
