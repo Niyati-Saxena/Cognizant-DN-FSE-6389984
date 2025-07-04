7135d7cb579c88707c07a886f6076d5cfef35c3a
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-07-04 08:53:33 +0530

    Create Country.java

---
+++ Week3/Spring_Data_JPA_with_SpringBoot_Hibernate/ex1_Spring_Data_JPA_Quick_example/Country.java
@@ -0,0 +1,42 @@
+package com.cognizant.orm_learn.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id  // marks it as primary key
+    @Column(name= "co_code") // maps to the column in db with same name
+    private String id;
+
+    @Column(name = "co_name")
+    private String name;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
