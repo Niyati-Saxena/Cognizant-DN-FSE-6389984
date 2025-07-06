6e525f6824c2f577769a516b8f07ada9d1455aa5
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-07-06 09:07:10 +0530

    Create User.java

---
+++ Week3/Spring_Data_JPA_with_SpringBoot_Hibernate/ex4_Difference_between_JPA_Hibernate_and_SpringDataJPA/Spring_Data_JPA/User.java
@@ -0,0 +1,39 @@
+package com.example.SprindDataJPA.entity;
+
+import jakarta.persistence.*;
+// same as used in hibernate
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+
+    public User() {}
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
