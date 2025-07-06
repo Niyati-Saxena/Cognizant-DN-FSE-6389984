c79f4deb91a184b6c27e9ec45c9be1a361321423
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-07-06 09:00:30 +0530

    Update Main.java

--- Week3/Spring_Data_JPA_with_SpringBoot_Hibernate/ex4_Difference_between_JPA_Hibernate_and_SpringDataJPA/Spring_Data_JPA/Main.java
+++ Week3/Spring_Data_JPA_with_SpringBoot_Hibernate/ex4_Difference_between_JPA_Hibernate_and_SpringDataJPA/Spring_Data_JPA/Main.java
@@ -1,1 +1,15 @@
-// java file
+
+package com.example.SprindDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprindDataJpaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SprindDataJpaApplication.class, args);
+	}
+
+}
