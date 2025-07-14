8abb9fabb38cf84bb23e343ce54dbc21a006d1bc
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-07-14 07:59:12 +0530

    Create JwtUtil.java

---
+++ Week4/JWT/util/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.cognizant.SpringJWT.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String secret = "mySecretKey"; // Keep it safe in env config
+    private final long expirationMs = 600000; // 10 minutes
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
