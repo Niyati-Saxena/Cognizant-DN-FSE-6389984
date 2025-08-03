45b3cd110c7abe6adaaf89d71d86db483bfa002d
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-08-03 22:42:19 +0530

    Create FlightDetails.js

---
+++ Week7/12.ReactJS-HOL/code/FlightDetails.js
@@ -0,0 +1,24 @@
+// created a fake api to fetch the data from 
+
+const FlightDetails = [
+  {
+    flightNumber: "AI203",
+    airline: "Air India",
+    departure: "Delhi",
+    status: "On Time",
+  },
+  {
+    flightNumber: "6E542",
+    airline: "IndiGo",
+    departure: "Bangalore",
+    status: "Delayed",
+  },
+  {
+    flightNumber: "UK880",
+    airline: "Vistara",
+    departure: "Kolkata",
+    status: "On Time",
+  }
+];
+
+export default FlightDetails;
